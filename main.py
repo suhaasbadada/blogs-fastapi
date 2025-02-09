e875c99975f16a7de94a000f9a440c667e71eb5d
Suhaas Rao Badada <suhaasbadada@gmail.com>
2025-02-09 10:36:35 -0500

    some structuring

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app=FastAPI()
@@ -6,3 +7,25 @@ app=FastAPI()
 def index():
     return {'data':{'name':'Suhaas'}}
 
+
+@app.get('/blog')
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data':f'{limit} published blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from the db'}
+
+
+@app.get('/about')
+def about():
+    return {'data':'about page'}
+
+
+@app.get('/blog/{id}')
+def show_blog(id: int):
+    return {'data':id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data':{'comment 1','comment 2'}}
